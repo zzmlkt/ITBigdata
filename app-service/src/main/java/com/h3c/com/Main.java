13d7444cb45ed8fa1feab77d19b7519031efa92e
zhengmk <zhengmk>
2017-08-15 23:42:29 +0800

    首次

---
+++ app-service/src/main/java/com/h3c/com/Main.java
@@ -0,0 +1,11 @@
+package com.h3c.com;
+
+/**
+ * Created by zhengmk on 2017/8/15.
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("hello world");
+    }
+}
